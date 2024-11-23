f51ac3bbeb13e4ae68b03af35dcd41841c182b5a
luc.perard@hotmail.fr <116542956+MonLucCo@users.noreply.github.com>
2024-11-23 12:59:24 +0100

    Etape0b_Configuration des fichiers d'installation

---
+++ cv-online/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development', // ou 'production' selon votre besoin
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                            sassOptions: {
+                                fiber: false,
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192, // Taille limite en octets pour les fichiers à intégrer sous forme de base64
+                            name: '[path][name].[ext]',
+                            publicPath: 'assets/images/',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss'],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+};
